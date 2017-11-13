9854d7de6981f00df4949a7438d0839962e4c19e
Jeferson Cunha <dev@jef.ninja>
2017-11-12 23:10:31 -0200

    test deploy heroku

--- server.js
+++ server.js
@@ -75,6 +75,6 @@ function onNewNamespace(channel, sender) {
 
 // app.listen(8888);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
