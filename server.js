90bc028c0480a71665905cd89d4361c9c40aba2e
Jeferson Cunha <dev@jef.ninja>
2017-11-15 19:10:50 -0200

    heroku test

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ function onNewNamespace(channel, sender) {
 //   console.log(`Online! Acåcess: ${host}`)
 // });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 app.listen(port, function () {
   var addr = app.address();
   console.log('   app listening on http://' + addr.address + ':' + addr.port);
